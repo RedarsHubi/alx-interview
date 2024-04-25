46365e8f581beecbaddcfd0f28a0ecbcc91ec0e2
RedarsHubi <redamasters@gmail.com>
2024-04-25 14:41:20 +0100

    Pycodestyle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,23 +3,22 @@
 '''
 
 
-
 def pascal_triangle(n):
-    list_of_lists = []
+    lista = []
 
     # Generate Pascal's Triangle
     for i in range(n):
         row = [1]  # Start each row with 1
         for j in range(1, i):  # Generate elements in between 1 and 1
-            row.append(list_of_lists[i - 1][j - 1] + list_of_lists[i - 1][j])
+            row.append(lista[i - 1][j - 1] + lista[i - 1][j])
         if i > 0:  # Skip for the first row
             row.append(1)  # End each row with 1
-        list_of_lists.append(row)
+        lista.append(row)
 
     # Modify odd rows after the third row
     for i in range(3, n):
         if i % 2 == 1:
-            mid_index = len(list_of_lists[i]) // 2
-            list_of_lists[i][mid_index] = list_of_lists[i - 1][mid_index - 1] + list_of_lists[i - 1][mid_index]
+            mid = len(lista[i]) // 2
+            lista[i][mid] = lista[i - 1][mid - 1] + lista[i - 1][mid]
 
-    return(list_of_lists)
+    return (lista)
