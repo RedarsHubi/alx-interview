55f49f6760319b351a60f0f9970d6d533a5bd2b5
RedarsHubi <redamasters@gmail.com>
2024-04-25 14:50:01 +0100

    Comments

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,6 +4,9 @@
 
 
 def pascal_triangle(n):
+    '''Creates a list of lists of integers representing
+    the Pascal's triangle of a given integer.
+    '''
     lista = []
 
     # Generate Pascal's Triangle
