098a079957be0b98af5c65fd62456b87a665fb30
RedarsHubi <redamasters@gmail.com>
2024-05-09 16:24:35 +0100

    .

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,32 +5,16 @@ Module for calculating most efficient way to copy and paste
 
 
 def minOperations(n):
-    """Returns minium num of operations"""
-    store = 0
-    value = 1
-    if (n <= 1):
+    """Returns minimum num of operations"""
+    if n <= 1:
         return 0
-    if (n % 2 == 0):
-        while value < n:
-            store += 2
-            value = value * 2
-            if (value == n):
-                return store
-            elif value * 2 > n:
-                value += value // 2
-                store += 1
-                return store
-    elif n % 3 == 0:
-        while value < n:
-            if value % 2 == 0:
-                value += value // 2
-                store += 1
-            else:
-                value *= 2
-                store += 2
-        return store
-    else:
-        while value < n:
-            store += 1
-            value *= 2
-        return store
+    operations = 0
+    divisor = 2
+    while n > 1:
+        if n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        else:
+            divisor += 1
+
+    return operations
