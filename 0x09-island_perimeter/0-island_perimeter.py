58af213b15e04cd3190f7a9f3df0f27159ef09c8
RedarsHubi <redamasters@gmail.com>
2024-06-25 10:28:47 +0100

    Sol

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -3,13 +3,16 @@
 
 def island_perimeter(grid):
     """Solution"""
-    res = 0
-
-    for row in grid:
-        for cell in row:
-            if cell == 1:
-                res += 2
-    if res != 0:
-        res += 2
-    
-    return res
+    p = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if (grid[i][j] == 1):
+                if (i <= 0 or grid[i - 1][j] == 0):
+                    p += 1
+                if (i >= len(grid) - 1 or grid[i + 1][j] == 0):
+                    p += 1
+                if (j <= 0 or grid[i][j - 1] == 0):
+                    p += 1
+                if (j >= len(grid[i]) - 1 or grid[i][j + 1] == 0):
+                    p += 1
+    return p
